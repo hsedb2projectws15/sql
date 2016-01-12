83bdd6d542c7e8e0c69cbbfaf500f7a87341435b
David Mändlen <david.maendlen@bifroe.st>
2016-01-12 15:00:48 +0100

    fixing ordering problems

--- setup/setup.bat
+++ setup/setup.bat
@@ -11,9 +11,6 @@ REM create relevant tables and views
 db2 -tf setupDB.sql
 db2 -tf createViews.sql
 
-REM grant all rights to public 
-db2 -tf grantRights.sql
-
 REM insert from rawdata
 db2 -tf 01_insert_lecturer.sql
 db2 -tf 02_insert_dep.sql
@@ -22,3 +19,6 @@ db2 -tf 04_insert_prof.sql
 db2 -tf 05_insert_extern.sql
 db2 -tf 06_insert_event.sql
 db2 -tf 07_insert_lecture_events.sql
+
+REM grant all rights to public 
+db2 -tf grantRights.sql
