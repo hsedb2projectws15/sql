c6ab79e4a361c06eb8f89fdbcb5761848dd9c59b
David Mändlen <david.maendlen@bifroe.st>
2016-01-12 15:03:24 +0100

    fixing ordering problems and formatting

--- setup/02_insert_dep.sql
+++ setup/02_insert_dep.sql
@@ -1,17 +1,17 @@
 INSERT INTO DEPARTMENT (DEPARTMENT_SHORT, DEPARTMENT_NAME)
 SELECT FirstRow, concat
 (
-CASE FirstRow
-WHEN 'G' then 'Grundlagen'
-WHEN 'GS' then 'Graduate School'
-WHEN 'IFS' then 'Institut für Fremdsprachen'
-WHEN 'IT' then 'Informationstechnik'
-WHEN 'WI' then 'Wirtschaftsingenieurswesen'
-WHEN 'AN' then 'Angewandte Naturwissenschaften'
-WHEN 'HHZ' then 'HHZ'
+	CASE FirstRow
+	WHEN 'G' then 'Grundlagen'
+	WHEN 'GS' then 'Graduate School'
+	WHEN 'IFS' then 'Institut für Fremdsprachen'
+	WHEN 'IT' then 'Informationstechnik'
+	WHEN 'WI' then 'Wirtschaftsingenieurswesen'
+	WHEN 'AN' then 'Angewandte Naturwissenschaften'
+	WHEN 'HHZ' then 'HHZ'
 
-ELSE 'Not Available'
-END, ''
+	ELSE 'Not Available'
+	END, ''
 ) AS SecondRow
 FROM(
 	Select coalesce (LECDEPT, 'NA') as FirstRow 
